39970469d0693f6f1f1ca3834752532dc2ede779
Martin Gerhardy <mgerhardy@bigpoint.net>
2015-10-07 10:15:42 +0200

    document stuff and don't use linux platform specific stuff

--- Source/EclipseSourceCodeAccess/Private/EclipseSourceCodeAccessor.cpp
+++ Source/EclipseSourceCodeAccess/Private/EclipseSourceCodeAccessor.cpp
@@ -1,6 +1,13 @@
 #include "EclipseSourceCodeAccessPrivatePCH.h"
 #include "EclipseSourceCodeAccessor.h"
+#include "ModuleManager.h"
 #include "DesktopPlatformModule.h"
+
+#if WITH_EDITOR
+#include "Developer/HotReload/Public/IHotReload.h"
+#endif
+
+DEFINE_LOG_CATEGORY_STATIC(LogEclipseAccessor, Log, All);
 
 #define LOCTEXT_NAMESPACE "EclipseSourceCodeAccessor"
 
@@ -28,51 +35,53 @@ FText FEclipseSourceCodeAccessor::GetDescriptionText() const
 
 bool FEclipseSourceCodeAccessor::OpenSolution()
 {
-	FString FullPath;
-	FString SolutionFilenameWithoutExtension = TEXT("UE4");
-	FString CodeSolutionFile = SolutionFilenameWithoutExtension + TEXT(".cproject");
-
-	if (!FDesktopPlatformModule::Get()->GetSolutionPath(FullPath))
+	FString Filename = FPaths::GetBaseFilename(GetSolutionPath()) + ".cproject";
+	FString Directory = FPaths::GetPath(GetSolutionPath());
+	FString Solution = "\"" + Directory + "/" + Filename + "\"";
+	FString EclipsePath;
+	if (!CanRunEclipse(EclipsePath))
 	{
 		return false;
 	}
-	if (!FPaths::FileExists(FullPath))
-	{
-		return false;
-	}
-	FString Editor = FString(TEXT("eclipse"));
-	FString Args = FString(TEXT(""));
 
-	const FString NewFullPath = FString::Printf(TEXT("\"%s\""), *FullPath);
+	UE_LOG(LogEclipseAccessor, Warning, TEXT("FEclipseSourceCodeAccessor::OpenSolution: %s %s"), *EclipsePath, *Solution);
 
-	Args.Append(NewFullPath);
-	if (!FLinuxPlatformProcess::CreateProc(*Editor, *Args, true, true, false, nullptr, 0, nullptr, nullptr).IsValid())
+	FProcHandle Proc = FPlatformProcess::CreateProc(*EclipsePath, *Solution, true, false, false, nullptr, 0, nullptr, nullptr);
+	if(Proc.IsValid())
 	{
-		return false;
+		FPlatformProcess::CloseProc(Proc);
+		return true;
 	}
-	return true;
+	return false;
 }
 
 bool FEclipseSourceCodeAccessor::OpenFileAtLine(const FString& FullPath, int32 LineNumber, int32 ColumnNumber)
 {
-	TArray<FString> Files(FullPath);
+	const FString FileWithLine = FString::Printf(TEXT("%s:%i "), *FullPath, LineNumber);
+	TArray<FString> Files(FileWithLine);
 	return OpensourceFiles(Files);
 }
 
-bool FEclipseSourceCodeAccessor::OpenSourceFiles(const TArray<FString>& AbsoluteSourcePaths) 
+bool FEclipseSourceCodeAccessor::OpenSourceFiles(const TArray<FString>& AbsoluteSourcePaths)
 {
-	FString Editor = FString(TEXT("eclipse"));
+	FString EclipsePath;
+	if (!CanRunEclipse(EclipsePath))
+	{
+		return false;
+	}
 	FString Args = FString(TEXT("--launcher.timeout 60 --launcher.openFile "));
 	for (const FString& SourcePath : AbsoluteSourcePaths)
 	{
 		const FString NewSourcePath = FString::Printf(TEXT("\"%s\" "), *SourcePath);
 		Args.Append(NewSourcePath);
 	}
-	if (!(FLinuxPlatformProcess::CreateProc(*Editor, *Args, true, true, false, nullptr, 0, nullptr, nullptr).IsValid()))
+	FProcHandle Proc = FPlatformProcess::CreateProc(*EclipsePath, *Solution, true, false, false, nullptr, 0, nullptr, nullptr);
+	if(Proc.IsValid())
 	{
-		return false;
+		FPlatformProcess::CloseProc(Proc);
+		return true;
 	}
-	return true;
+	return false;
 }
 
 bool FEclipseSourceCodeAccessor::AddSourceFiles(const TArray<FString>& AbsoluteSourcePaths, const TArray<FString>& AvailableModules)
@@ -89,4 +98,44 @@ void FEclipseSourceCodeAccessor::Tick(const float DeltaTime)
 {
 }
 
-#undef LOCTEXT_NAMESPACE
+void FEclispeSourceCodeAccessor::CanRunEclipse(FString& OutPath) const
+{
+	// TODO This might be not a good idea to find an executable.
+	OutPath = TEXT("/usr/bin/eclipse");
+	if (!FPaths::FileExists(OutPath))
+	{
+		TCHAR EclipseBinaryEnv[32768] = { 0 };
+		FPlatformMisc::GetEnvironmentVariable(TEXT("UE4_ECLIPSE_BINARY"), EclipseBinaryEnv, ARRAY_COUNT(EclipseBinaryEnv));
+		OutPath = EclipseBinaryEnv;
+
+		if (!FPaths::FileExists(OutPath))
+		{
+			UE_LOG(LogEclipseAccessor, Warning, TEXT("FEclipseSourceCodeAccessor: Can not find eclipse binary - export UE4_ECLIPSE_BINARY environment variable"));
+			return false;
+		}
+	}
+	return true;
+}
+
+FString FEclipseSourceCodeAccessor::GetSolutionPath() const
+{
+	if (IsInGameThread())
+	{
+		FString SolutionPath;
+		if (FDesktopPlatformModule::Get()->GetSolutionPath(SolutionPath))
+		{
+			CachedSolutionPath = FPaths::ConvertRelativePathToFull(SolutionPath);
+		}
+	}
+	return CachedSolutionPath;
+}
+
+void FEclipseSourceCodeAccessor::Startup()
+{
+	// Cache this so we don't have to do it on a background thread
+	GetSolutionPath();
+}
+
+void FEclipseSourceCodeAccessor::Shutdown()
+{
+}
--- Source/EclipseSourceCodeAccess/Private/EclipseSourceCodeAccessor.h
+++ Source/EclipseSourceCodeAccess/Private/EclipseSourceCodeAccessor.h
@@ -4,15 +4,74 @@
 
 class FEclipseSourceCodeAccessor : public ISourceCodeAccessor
 {
+private:
+	/** String storing the solution path obtained from the module manager to avoid having to use it on a thread */
+	mutable FString CachedSolutionPath;
+
+	/** Gets solution path */
+	FString GetSolutionPath() const;
+
+	/** Tests if eclipse is present and returns path to it */
+	bool CanRunEclipse(FString& OutPath) const;
 public:
+	/**
+	 * Can we access source code.
+	 */
 	virtual bool CanAccessSourceCode() const override;
+
+	/**
+	 * Get the name of this accessor. Will be used as a unique identifier.
+	 */
 	virtual FName GetFName() const override;
+
+	/**
+	 * Get the name of this accessor.
+	 */
 	virtual FText GetNameText() const override;
+
+	/**
+	 * Get the descriptor of this accessor.
+	 */
 	virtual FText GetDescriptionText() const override;
+
+	/**
+	 * Open the CodeLite Workspace for editing.
+	 */
 	virtual bool OpenSolution() override;
+
+	/**
+	 * Open a file at a specific line and optional column.
+	 */
 	virtual bool OpenFileAtLine(const FString& FullPath, int32 LineNumber, int32 ColumnNumber = 0) override;
+
+	/**
+	 * Open a group of files.
+	 */
 	virtual bool OpenSourceFiles(const TArray<FString>& AbsoluteSourcePaths) override;
+
+	/**
+	 * Add a group of files.
+	 */
 	virtual bool AddSourceFiles(const TArray<FString>& AbsoluteSourcePaths, const TArray<FString>& AvailableModules) override;
+
+	/**
+	 * Save all open files.
+	 */
 	virtual bool SaveAllOpenDocuments() const override;
+
+	/**
+	 * Tick this source code accessor
+	 * @param DeltaTime Delta time (in seconds) since the last call to Tick
+	 */
 	virtual void Tick(const float DeltaTime) override;
+
+	/**
+	 * Initialize the accessor.
+	 */
+	void Startup();
+
+	/**
+	 * Deinitialize the accessor.
+	 */
+	void Shutdown();
 };
